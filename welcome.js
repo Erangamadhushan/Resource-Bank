1f703e146c69b3f0db75a5b77b351dc07ed3baae
Erangamadhushan <erangamadhushann956@gmail.com>
2024-11-28 18:59:57 +0530

    page is currently interrup for development purpose

--- welcome.js
+++ welcome.js
@@ -1,5 +1,5 @@
 // window.alert("Welcome to basics web pages with html,css and javascript");
 window.addEventListener("DOMContentLoaded", () => {
-    let loadingGif = document.querySelector('.loading');
-    loadingGif.style.display = "none";
+    // let loadingGif = document.querySelector('.loading');
+    // loadingGif.style.display = "none";
 })
